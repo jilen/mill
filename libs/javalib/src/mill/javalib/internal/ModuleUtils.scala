b010c7e9e664759b6ae41befa8a818c9eeda9ca3
jilen <jilen.zhang@gmail.com>
2026-05-04 22:12:24 +0800

    Optmize performance of ModuleUtils further.
    Seems better use mutable set

--- libs/javalib/src/mill/javalib/internal/ModuleUtils.scala
+++ libs/javalib/src/mill/javalib/internal/ModuleUtils.scala
@@ -1,8 +1,8 @@
 package mill.javalib.internal
 
 import mill.api.daemon.internal.internal
-
 import scala.annotation.tailrec
+import scala.collection.mutable.{LinkedHashSet, HashSet}
 
 @internal
 object ModuleUtils {
@@ -19,43 +19,26 @@ object ModuleUtils {
    */
   // FIXME: Remove or consolidate with copy in JvmWorkerImpl
   def recursive[T](name: String, start: T, deps: T => Seq[T]): Seq[T] = {
-
-    @tailrec def rec(
-        seenModules: Vector[T],
-        seenSet: Set[T],
-        toAnalyze: List[(List[T], Set[T], List[T])]
-    ): Vector[T] = {
-      toAnalyze match {
-        case Nil => seenModules
-        case traces :: rest =>
-          traces match {
-            case (_, _, Nil) => rec(seenModules, seenSet, rest)
-            case (trace, traceSet, cand :: remaining) =>
-              if (traceSet.contains(cand)) {
-                // cycle!
-                val rendered =
-                  (cand :: (cand :: trace.takeWhile(_ != cand)).reverse).mkString(" -> ")
-                val msg = s"${name}: cycle detected: ${rendered}"
-                println(msg)
-                throw new mill.api.MillException(msg)
-              }
-              val seen = seenSet.contains(cand)
-              rec(
-                if (seen) seenModules else seenModules :+ cand,
-                if (seen) seenSet else seenSet + cand,
-                toAnalyze =
-                  ((cand :: trace, traceSet + cand, deps(cand).toList)) ::
-                    (trace, traceSet, remaining) ::
-                    rest
-              )
-          }
+    val seen = LinkedHashSet.empty[T]
+    val traceSet = HashSet.empty[T]
+    
+    @tailrec
+    def visit(trace: List[T], node: T): Unit = {
+      if (traceSet.contains(node)) {
+        val rendered =
+          (node :: (node :: trace.takeWhile(_ != node)).reverse).mkString(" -> ")
+        throw new RuntimeException(s"${name}: cycle detected: ${rendered}")
+      }
+      if (!seen.contains(node)) {
+        seen += node
+        traceSet += node
+        deps(node).foreach(d => visit(node :: trace, d))
+        traceSet -= node
       }
     }
 
-    rec(
-      seenModules = Vector.empty,
-      seenSet = Set.empty,
-      toAnalyze = List((List(start), Set(start), deps(start).toList))
-    ).reverse
+    visit(Nil, start)
+    seen.toSeq
   }
+    
 }
